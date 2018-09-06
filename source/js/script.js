62c7ef3ab3a8c64bd1f24b14a1583dbd80c7ff21
nhomerc <39440448+nhomerc@users.noreply.github.com>
2018-09-06 22:24:15 +0300

    Глобально переделано меню, добавлен раздел уведомлений

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,25 @@
-var showHideButton = document.querySelector(".page-heade__button");
-var mainMenu = document.querySelector(".page-header__menu");
-var showHideMenu=document.querySelector(".page-header__menu-button");
+var showHideButton = document.querySelector(".page-header__button");
+var mainMenu = document.querySelector(".page-header__menu-list");
+var headerMobile = document.querySelector(".page-header__wapper");
+var logo = document.querySelector(".page-header__logo");
+var notice = document.querySelector(".page-header__notification-wrapper");
+var perconalRoom = document.querySelector(".page-header__personal-area");
+var phoneNumber = document.querySelector(".page-header__phone");
+
+headerMobile.classList.remove("show");
+mainMenu.classList.remove("show");
+showHideButton.classList.remove("open");
 
 showHideButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   mainMenu.classList.toggle("show");
-})
-showHideMenu.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  mainMenu.classList.remove("show");
+  showHideButton.classList.toggle("open");
+  headerMobile.classList.toggle("show");
+  logo.classList.toggle("open");
+  notice.classList.toggle("open");
+  perconalRoom.classList.toggle("open");
+  phoneNumber.classList.toggle("open");
 })
 
-showHideButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  showHideButton.classList.toggle("open");
-})
+
+
